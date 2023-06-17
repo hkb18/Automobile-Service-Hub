9405d97afd4d5f1088d0011b11a52045ce13b25b
hkb <hitheshkbimal2000@gmail.com>
2023-06-17 22:31:18 +0530

    Admin Add details of AirPurifier

--- app/src/main/java/com/example/auton/admin_add_AirPurifier.java
+++ app/src/main/java/com/example/auton/admin_add_AirPurifier.java
@@ -1,24 +1,47 @@
 package com.example.auton;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.airbnb.lottie.L;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class admin_add_AirPurifier extends AppCompatActivity {
-    Button add;
+    Button add,selectImg;
+    ProgressDialog progressDialog;
+    ProgressBar progressBar;
+    ImageView imageView;
+    StorageReference storageReference;
+    Uri imageUri;
+    String fileName;
     DatabaseReference databaseReference;
     TextInputEditText textInputEditTextModel,textInputEditTextVoltage,textInputEditTextColor,textInputEditTextDimension,textInputEditTextWeight,textInputEditTextManufacturer,textInputEditTextWarrenty,textInputEditTextItemIncluded,textInputEditTextPrice,textInputEditTextQuantity;
     String modelStr,operatingVoltageStr,colorStr,dimensionStr,weightStr,manufacturerStr,warrentyStr,itemsIncludedStr,priceStr,quantityStr;
@@ -28,6 +51,10 @@ public class admin_add_AirPurifier extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_air_purifier);
+
+        progressBar = new ProgressBar(this);
+        imageView=findViewById(R.id.ivAirpurifier);
+        selectImg=findViewById(R.id.btn_selectImgAirPurifier);
 
         add=findViewById(R.id.btn_add_AirPurifier);
         textInputEditTextModel=findViewById(R.id.airpurifierModel);
@@ -62,20 +89,10 @@ public class admin_add_AirPurifier extends AppCompatActivity {
                     databaseReference.child("Accessories").addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot snapshot) {
-                            databaseReference.child("Accessories").child("AirPurifier").child(modelStr).child("Model").setValue(modelStr);
-                            databaseReference.child("Accessories").child("AirPurifier").child(modelStr).child("OperatingVoltage").setValue(operatingVoltageStr);
-                            databaseReference.child("Accessories").child("AirPurifier").child(modelStr).child("Color").setValue(colorStr);
-                            databaseReference.child("Accessories").child("AirPurifier").child(modelStr).child("Dimenension").setValue(dimensionStr);
-                            databaseReference.child("Accessories").child("AirPurifier").child(modelStr).child("Weight").setValue(weightStr);
-                            databaseReference.child("Accessories").child("AirPurifier").child(modelStr).child("Manufacturer").setValue(manufacturerStr);
-                            databaseReference.child("Accessories").child("AirPurifier").child(modelStr).child("Warrenty").setValue(warrentyStr);
-                            databaseReference.child("Accessories").child("AirPurifier").child(modelStr).child("ItemsIncluded").setValue(itemsIncludedStr);
-                            databaseReference.child("Accessories").child("AirPurifier").child(modelStr).child("Price").setValue(priceStr);
-                            databaseReference.child("Accessories").child("AirPurifier").child(modelStr).child("Quantity").setValue(quantityStr);
+                            uploadImage();
 
-//                            databaseReference.child("Accessories").child("AndroidScreens").child(modelStr).child("Image").setValue();
                             Toast.makeText(admin_add_AirPurifier.this, "Value Entered", Toast.LENGTH_SHORT).show();
-                            Intent i = new Intent(getApplicationContext(), admin_Add_Accessories.class);
+                            Intent i = new Intent(getApplicationContext(), admin_add_Carcare_Purifier.class);
                             startActivity(i);
                         }
 
@@ -85,6 +102,108 @@ public class admin_add_AirPurifier extends AppCompatActivity {
                         }
                     });
                 }
+            }
+        });
+    //      IMG UPLOAD
+        selectImg.setOnClickListener(v -> {
+            selectImage();
+        });
+}
+    private void selectImage(){
+        Intent intent=new Intent();
+        intent.setType("image/+");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==100 && data != null && data.getData() != null){
+            imageUri=data.getData();
+            imageView.setImageURI(imageUri);
+        }
+    }
+    private void uploadImage() {
+        if (imageUri != null) {
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading File...");
+            progressDialog.show();
+
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.CANADA);
+            Date now = new Date();
+            fileName = formatter.format(now);
+            storageReference = FirebaseStorage.getInstance().getReference("images/" + fileName);
+
+            storageReference.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    imageView.setImageURI(null);
+                    Toast.makeText(admin_add_AirPurifier.this, "Successfully Uploaded", Toast.LENGTH_SHORT).show();
+                    if (progressDialog.isShowing())
+                        progressDialog.dismiss();
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                    if (progressDialog.isShowing())
+                        progressDialog.dismiss();
+
+                    Toast.makeText(admin_add_AirPurifier.this, "Failed to Upload", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+            uploadtoFirebase(imageUri);
+        }
+        else {
+            Toast.makeText(this, "Please select Image", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void uploadtoFirebase(Uri uri) {
+
+
+        //storageReference= storageReference.child(System.currentTimeMillis()+"."+getFileExtension(uri));
+        storageReference=storageReference.child("images/").child(fileName);
+        storageReference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        databaseReference.child("Accessories").child("CARCARE_PURIFIERS").child("AirPurifier").child(modelStr).child("Image").setValue(uri.toString());
+                        databaseReference.child("Accessories").child("CARCARE_PURIFIERS").child("AirPurifier").child(modelStr).child("Model").setValue(modelStr);
+                        databaseReference.child("Accessories").child("CARCARE_PURIFIERS").child("AirPurifier").child(modelStr).child("OperatingVoltage").setValue(operatingVoltageStr);
+                        databaseReference.child("Accessories").child("CARCARE_PURIFIERS").child("AirPurifier").child(modelStr).child("Color").setValue(colorStr);
+                        databaseReference.child("Accessories").child("CARCARE_PURIFIERS").child("AirPurifier").child(modelStr).child("Dimenension").setValue(dimensionStr);
+                        databaseReference.child("Accessories").child("CARCARE_PURIFIERS").child("AirPurifier").child(modelStr).child("Weight").setValue(weightStr);
+                        databaseReference.child("Accessories").child("CARCARE_PURIFIERS").child("AirPurifier").child(modelStr).child("Manufacturer").setValue(manufacturerStr);
+                        databaseReference.child("Accessories").child("CARCARE_PURIFIERS").child("AirPurifier").child(modelStr).child("Warrenty").setValue(warrentyStr);
+                        databaseReference.child("Accessories").child("CARCARE_PURIFIERS").child("AirPurifier").child(modelStr).child("ItemsIncluded").setValue(itemsIncludedStr);
+                        databaseReference.child("Accessories").child("CARCARE_PURIFIERS").child("AirPurifier").child(modelStr).child("Price").setValue(priceStr);
+                        databaseReference.child("Accessories").child("CARCARE_PURIFIERS").child("AirPurifier").child(modelStr).child("Quantity").setValue(quantityStr);
+                        Log.e("TAG", "onSuccess: "+manufacturerStr+modelStr+operatingVoltageStr+colorStr+dimensionStr+weightStr+warrentyStr+itemsIncludedStr+priceStr+quantityStr );
+                        Toast.makeText(admin_add_AirPurifier.this, "Uploaded Successfully ", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                progressBar.setVisibility(View.VISIBLE);
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressBar.setVisibility(View.INVISIBLE);
+                Toast.makeText(admin_add_AirPurifier.this, "Uploading Failed !!", Toast.LENGTH_SHORT).show();
             }
         });
     }
