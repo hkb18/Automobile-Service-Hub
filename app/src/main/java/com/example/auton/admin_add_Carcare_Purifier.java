965b272da4906b77aec5e41b00d49f8cb79e9691
hkb <hitheshkbimal2000@gmail.com>
2023-06-28 22:57:00 +0530

    used binding for admin_add_Carcare_Purifier.java & admin_add_ScreenSpeaker.java

--- app/src/main/java/com/example/auton/admin_add_Carcare_Purifier.java
+++ app/src/main/java/com/example/auton/admin_add_Carcare_Purifier.java
@@ -7,71 +7,44 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.auton.databinding.ActivityAdminAddCarcarePurifierBinding;
+
 public class admin_add_Carcare_Purifier extends AppCompatActivity {
-    Button airpurifier,cleaningkit,microfibers,wiperblades,cleansers,detailing,washers;
+    private ActivityAdminAddCarcarePurifierBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_add_carcare_purifier);
+        binding=ActivityAdminAddCarcarePurifierBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        airpurifier=findViewById(R.id.btn_AirPurifiers);
-        cleaningkit=findViewById(R.id.btn_CleaningKits);
-        microfibers=findViewById(R.id.btn_Microfibers);
-        wiperblades=findViewById(R.id.btn_WiperBlades);
-        cleansers=findViewById(R.id.btn_Cleaners);
-        detailing=findViewById(R.id.btn_Detailing);
-        washers=findViewById(R.id.btn_Washers);
-
-        airpurifier.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),admin_add_AirPurifier.class);
-                startActivity(i);
-            }
+        binding.btnAirPurifiers.setOnClickListener(view -> {
+            Intent i = new Intent(getApplicationContext(),admin_add_AirPurifier.class);
+            startActivity(i);
         });
-        cleaningkit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),admin_add_CleaningKit.class);
-                startActivity(i);
-            }
+        binding.btnCleaners.setOnClickListener(view -> {
+            Intent i = new Intent(getApplicationContext(),admin_add_Cleansers.class);
+            startActivity(i);
         });
-        microfibers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),admin_add_MicroFibres.class);
-                startActivity(i);
-            }
+        binding.btnCleaningKits.setOnClickListener(view -> {
+            Intent i = new Intent(getApplicationContext(),admin_add_CleaningKit.class);
+            startActivity(i);
         });
-        wiperblades.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),admin_add_WiperBlades.class);
-                startActivity(i);
-            }
+        binding.btnMicrofibers.setOnClickListener(view -> {
+            Intent i = new Intent(getApplicationContext(),admin_add_MicroFibres.class);
+            startActivity(i);
         });
-        cleansers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),admin_add_Cleansers.class);
-                startActivity(i);
-            }
+        binding.btnWiperBlades.setOnClickListener(view -> {
+            Intent i = new Intent(getApplicationContext(),admin_add_WiperBlades.class);
+            startActivity(i);
         });
-
-        detailing.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),admin_add_Detailing.class);
-                startActivity(i);
-            }
+        binding.btnDetailing.setOnClickListener(view -> {
+            Intent i = new Intent(getApplicationContext(),admin_add_Detailing.class);
+            startActivity(i);
         });
-        washers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),admin_add_Washers.class);
-                startActivity(i);
-            }
+        binding.btnWashers.setOnClickListener(view -> {
+            Intent i = new Intent(getApplicationContext(),admin_add_Washers.class);
+            startActivity(i);
         });
     }
 }
--- app/src/main/java/com/example/auton/admin_add_ScreenSpeaker.java
+++ app/src/main/java/com/example/auton/admin_add_ScreenSpeaker.java
@@ -7,58 +7,40 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.auton.databinding.ActivityAdminAddScreenSpeakerBinding;
+
 public class admin_add_ScreenSpeaker extends AppCompatActivity {
-    Button amplifier,vacuumcleaner,speakers,basstubes,androidscreen;
+    private ActivityAdminAddScreenSpeakerBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_add_screen_speaker);
+        binding=ActivityAdminAddScreenSpeakerBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-
-        amplifier=findViewById(R.id.btn_amplifier);
-        vacuumcleaner=findViewById(R.id.btn_VacuumCleaners);
-        speakers=findViewById(R.id.btn_Speakers);
-        basstubes=findViewById(R.id.btn_BassTubes);
-        androidscreen=findViewById(R.id.btn_AndroidScreens);
-
-        androidscreen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i=new Intent(getApplicationContext(),admin_add_AndroidScreen.class);
-                startActivity(i);
-            }
+        binding.btnAmplifier.setOnClickListener(view -> {
+            Intent i=new Intent(getApplicationContext(),admin_add_Amplifiers.class);
+            startActivity(i);
         });
 
-        basstubes.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i=new Intent(getApplicationContext(),admin_add_BassTubes.class);
-                startActivity(i);
-            }
+        binding.btnAndroidScreens.setOnClickListener(view -> {
+            Intent i=new Intent(getApplicationContext(),admin_add_AndroidScreen.class);
+            startActivity(i);
         });
 
-        speakers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i=new Intent(getApplicationContext(),admin_add_Speakers.class);
-                startActivity(i);
-            }
+        binding.btnBassTubes.setOnClickListener(view -> {
+            Intent i=new Intent(getApplicationContext(),admin_add_BassTubes.class);
+            startActivity(i);
         });
 
-        vacuumcleaner.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i=new Intent(getApplicationContext(),admin_add_VacuumCleaners.class);
-                startActivity(i);
-            }
+        binding.btnSpeakers.setOnClickListener(view -> {
+            Intent i=new Intent(getApplicationContext(),admin_add_Speakers.class);
+            startActivity(i);
         });
 
-        amplifier.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i=new Intent(getApplicationContext(),admin_add_Amplifiers.class);
-                startActivity(i);
-            }
+        binding.btnVacuumCleaners.setOnClickListener(view -> {
+            Intent i=new Intent(getApplicationContext(),admin_add_VacuumCleaners.class);
+            startActivity(i);
         });
+
     }
 }
