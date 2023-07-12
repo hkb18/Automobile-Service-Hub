0a614cd2d172d9dde3bc6d27216c4e20cd947d71
hkb <hitheshkbimal2000@gmail.com>
2023-07-12 16:53:01 +0530

    admin logout

--- app/src/main/java/com/example/auton/admin_HomePage.java
+++ app/src/main/java/com/example/auton/admin_HomePage.java
@@ -3,19 +3,60 @@ package com.example.auton;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class admin_HomePage extends AppCompatActivity {
-
+ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home_page);
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_nav_admin);
+        imageView = findViewById(R.id.imageviewLogout);
+
+        AlertDialog.Builder builder=new AlertDialog.Builder(admin_HomePage.this);
+
+        imageView.setOnClickListener(view -> {
+            builder.setMessage("Are you sure you want to logout?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            finishAffinity(); //ADD TO WORKSHOP N ADMIN
+
+                            SharedPreferences loginPref;
+                            SharedPreferences.Editor loginPrefEditor;
+                            loginPref = getSharedPreferences("login", MODE_PRIVATE);
+                            loginPrefEditor =loginPref.edit();
+                            loginPrefEditor.putBoolean("isLogin", false);
+                            loginPrefEditor.apply();
+                            Toast.makeText(admin_HomePage.this, "You have been Logged Out", Toast.LENGTH_SHORT).show();
+                            Intent i =new Intent(admin_HomePage.this,MainActivity.class);
+                            startActivity(i);
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                            Toast.makeText(admin_HomePage.this, "You choose no action for Alertbox", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+            AlertDialog alert=builder.create();
+            alert.setTitle("User Logout");
+            alert.show();
+
+        });
+
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_admin,new admin_Add_Accessories()).commit();
